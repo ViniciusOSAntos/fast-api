2b8601eefd98198448f48b1f25f34ff0a69e67c9
Vinicius Oliveira dos Santos <43036615+ViniciusOSAntos@users.noreply.github.com>
2023-06-18 18:35:50 -0300

    studies from 17/06

--- secao003/main.py
+++ secao003/main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi import Response
 from fastapi import HTTPException
 from fastapi import status
-from  models import Curso
+from fastapi import Path, Query, Header, Depends
 
-app = FastAPI()
+from  models import Curso
+from typing import Any, Dict, List
+from time import sleep
+
+def fake_db():
+    try:
+        print('Abrindo conxão com banco de dados...')
+        sleep(1)
+    finally:
+        print('Fechando conexão com o banco de dados...')
+        sleep(1)
+
+app = FastAPI(
+    title='Studies FastAPI',
+    version='0.01',
+    description='API for FastAPI studies'
+    )
 
 cursos = {
    1: {
@@ -19,12 +37,23 @@ cursos = {
 }
 
 
-@app.get('/cursos')
-async def get_cursos():
+@app.get('/cursos',
+         description='Retorna todos os cursos ou uma lista vazia',
+         summary='Retorna todos os cursos',
+         response_model=List[Curso],
+         response_description="Cursos Retornados com Sucesso "
+         )
+async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
 
 @app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+async def get_curso(curso_id: int = Path( 
+    title='ID do Curso', 
+    description='Deve ser entre 1 e 2', 
+    gt=0, 
+    lt=3)
+    ): # Tem que ser maior que 0 e menor que 3
+
     try:     
         curso = cursos[curso_id]
         curso.update({"id": curso_id})
@@ -51,6 +80,24 @@ async def put_curso(curso_id: int, curso: Curso):
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Não existe um curso com id {curso_id}')
+
+@app.delete('/cursos/{curso_id}')
+async def delete_curso(curso_id: int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT) Esse é o método ideal porém há um big na lib
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Não existe um curso com id {curso_id}")
+
+
+@app.get('/calculadora')
+async def calcular(x_geek: str = Header(default=None),a: int = Query(gt=5), b: int = Query(gt=10),  c: int = Query(gt=15)):
+    resultado = a + b + c
+
+    print(f"X-GEEK: {x_geek}")
+    return {"resultado": resultado}
 
 if __name__ == '__main__':
     import uvicorn
